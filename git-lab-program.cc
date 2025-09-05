ecd2ec8cd0151f97741369eea87b4cc91e8c61bf
Landon <ls641924@ohio.edu>
2025-09-05 17:49:03 -0400

    idk

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Landon Solether>
+ *        Date: <September 5, 2025>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
